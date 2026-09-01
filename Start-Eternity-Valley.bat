9ed5009458b9ff3f8ec08b5e1530f6018aa61314
JOTSmedia <johnnybaab@gmail.com>
2026-09-01 04:08:38 -0400

    d

--- Start-Eternity-Valley.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ========================================================
-echo  Starting Eternity Valley...
-echo  Opening in your default browser: http://localhost:8000/
-echo ========================================================
-start "" http://localhost:8000/
-python -m http.server 8000 || python3 -m http.server 8000 || node server\server.js
-pause
