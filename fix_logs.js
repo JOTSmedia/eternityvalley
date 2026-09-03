e8612ccaa3ae7c5d09d9a7d2e31e5655899ecc4d
JOTSmedia <johnnybaab@gmail.com>
2026-09-03 18:12:35 -0400

    dl

--- fix_logs.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('js/WorldTerrain.js', 'utf8');
-code = code.replace(/console\.log\("\[world3d\] _terrain Pass 2 done"\);/g, 'console.log("[world3d] _terrain Pass 2 done", performance.now());');
-code = code.replace(/console\.log\("\[world3d\] mesh added to scene"\);/g, 'console.log("[world3d] mesh added to scene", performance.now());');
-code = code.replace(/console\.log\('\[world3d\] _terrain Pass 2 start'\);/g, 'console.log("[world3d] _terrain Pass 2 start", performance.now());');
-fs.writeFileSync('js/WorldTerrain.js', code);
